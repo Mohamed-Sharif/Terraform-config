2f02c237355a4b924af9f063a20b633f314d531b
Mohamed Sharif <68668775+Mohamed-Sharif@users.noreply.github.com>
2023-04-21 06:53:12 +0200

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_all_ssh" {
   name        = "allow_all_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     description      = "SSH from VPC"
@@ -9,7 +9,6 @@ resource "aws_security_group" "allow_all_ssh" {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -28,11 +27,34 @@ resource "aws_security_group" "allow_all_ssh" {
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     description = "SSH from VPC"
     from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
+            }
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.my_vpc.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = [aws_vpc.main.cidr_block]
